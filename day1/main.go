92434a91d83b00a17e86e3b34c2bfbb55ddeef0f
Ell534 <elliot94b@gmail.com>
2023-12-07 17:39:32 +0000

    FINALLY get day 1 part 2...I am silly...

--- day1/main.go
+++ day1/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 	"unicode"
 )
 
@@ -20,6 +21,19 @@ var unicodeToNumber = map[byte]int{
 	55: 7,
 	56: 8,
 	57: 9,
+}
+
+var wordToNumber = map[string]string{
+	"zero":  "z0ero",
+	"one":   "o1ne",
+	"two":   "t2wo",
+	"three": "t3hree",
+	"four":  "f4our",
+	"five":  "f5ive",
+	"six":   "s6ix",
+	"seven": "s7even",
+	"eight": "e8ight",
+	"nine":  "n9ine",
 }
 
 // take the input
@@ -42,64 +56,70 @@ func fileReader(pathToFile string) []string {
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
-		// fmt.Println(scanner.Text())
 		scanSlice = append(scanSlice, scanner.Text())
 	}
-	//	fmt.Println(len(scanSlice))
 	return scanSlice
 }
 
-func digitExtract(stringSlice []string) []string {
-
-	var digitSlice = make([]string, len(stringSlice))
-	for wordIndex, word := range stringSlice {
-		for _, char := range word {
-			if unicode.IsDigit(char) {
-				digitSlice[wordIndex] = digitSlice[wordIndex] + string(char)
-			}
+func wordToDigit(stringSlice []string) []string {
+	for key, value := range wordToNumber {
+		for i, string := range stringSlice {
+			stringSlice[i] = strings.ReplaceAll(string, key, value)
 		}
 	}
-
-	for i, word := range digitSlice {
-		if len(word) == 1 {
-			digitSlice[i] += digitSlice[i]
-		}
-	}
-	// fmt.Println(digitSlice)
-	// fmt.Printf("%T\n", digitSlice[0])
-	return digitSlice
-}
-
-func twoDigits(stringSlice []string) []string {
-	fmt.Println("hello from twoDigits")
-	for i, digits := range stringSlice {
-		if len(digits) > 2 {
-			stringSlice[i] = string(digits[0]) + string(digits[len(digits)-1])
-		}
-	}
-	// fmt.Println(stringSlice)
 	return stringSlice
 }
 
-func strToInt(stringSlice []string) int {
-
-	var sumOfValues = 0
-
-	for _, digits := range stringSlice {
-		intValue, err := strconv.Atoi(digits)
-		if err != nil {
-			fmt.Printf("Atoi error:\n %d", err)
+func extractDigits(stringSlice []string) []string {
+	stringDigits := make([]string, len(stringSlice))
+	for stringSliceIndex, word := range stringSlice {
+		for _, char := range word {
+			if unicode.IsDigit(char) {
+				stringDigits[stringSliceIndex] = stringDigits[stringSliceIndex] + string(char)
+			}
 		}
-		sumOfValues += intValue
 	}
-	fmt.Println("Hopefully this works!")
-	fmt.Println(sumOfValues)
-	return sumOfValues
+	return stringDigits
+}
+
+func getFirstAndLast(sliceStringDigits []string) []string {
+	firstAndLast := make([]string, len(sliceStringDigits))
+
+	for index, element := range sliceStringDigits {
+		firstAndLast[index] = string(element[0]) + string(element[len(element)-1])
+	}
+	return firstAndLast
+}
+
+func convToInt(sliceDigits []string) ([]int, error) {
+	intSlice := make([]int, len(sliceDigits))
+
+	for i, value := range sliceDigits {
+		int, err := strconv.Atoi(value)
+		if err != nil {
+			return nil, err
+		}
+		intSlice[i] = int
+	}
+	return intSlice, nil
+}
+
+func sumInts(intSlice []int) int {
+	sum := 0
+
+	for _, value := range intSlice {
+		sum += value
+	}
+	fmt.Println(sum)
+	return sum
 }
 
 func main() {
-	textSlice := fileReader("sample.txt")
-	digitSlice := digitExtract(textSlice)
-	twoDigitSlice := twoDigits(digitSlice)
-	strToInt(twoDigitSlice)
+	fmt.Println("Main!")
+	words := fileReader("sample.txt")
+	wordToDigitsSlice := wordToDigit(words)
+	extractedDigits := extractDigits(wordToDigitsSlice)
+	firstAndLast := getFirstAndLast(extractedDigits)
+	convertedInts, _ := convToInt(firstAndLast)
+	sumInts(convertedInts)
 }
